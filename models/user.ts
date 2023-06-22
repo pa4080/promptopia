d2ecba3759968297aa2348f6f6fa0b526fa7749a
Spas Z. Spasov <spas.z.spasov@metalevel.tech>
2023-06-22 12:15:31 +0300

    feat: setup git-hub login with scope of the data

--- models/user.ts
+++ models/user.ts
@@ -12,8 +12,8 @@ const UserSchema = new Schema({
 		type: String,
 		required: [true, "Username is required!"],
 		match: [
-			/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-			"Username is invalid, it should contain 8-20 alphanumeric letters and be unique!",
+			/^(?=.{4,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+			"Username is invalid, it should contain 4-20 alphanumeric letters and be unique!",
 		],
 	},
 	name: {
--- next.config.js
+++ next.config.js
@@ -21,6 +21,12 @@ const nextConfig = {
 				port: "",
 				pathname: "/**",
 			},
+			{
+				protocol: "https",
+				hostname: "avatars.githubusercontent.com",
+				port: "",
+				pathname: "/**",
+			},
 		],
 	},
 	/**
