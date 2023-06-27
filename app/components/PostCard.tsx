02c58ab456db996b9e7c55c281a86440f27d688e
Spas Z. Spasov <spas.z.spasov@metalevel.tech>
2023-06-27 21:04:17 +0300

    feat: polish the post cards grid

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -9,7 +9,8 @@ import { useTranslations } from "next-intl";
 import { PostTypeFromDb } from "@/interfaces/Post";
 import logo from "@/public/icons/svg/mlt.promptopia.logo.favicon.svg";
 
-import IconEmbedSVG from "./fragments/IconEmbedSVG";
+import PostTag from "./fragments/PostTag";
+import IconEmbedSvgPop from "./fragments/IconEmbedSvgPop";
 
 interface Props {
 	post: PostTypeFromDb;
@@ -28,9 +29,9 @@ export const PostCard: React.FC<Props> = ({ post, handleTagClick }) => {
 
 	return (
 		<div className="prompt_card">
-			<div className="flex_between_start gap-5">
-				<div className="flex flex-1 justify-start items-center gap-3 cursor-pointer">
-					<div className="flex_center w-14 h-14 cursor-pointer rounded-full z-10 bg-white">
+			<div className="relative">
+				<div className="flex flex-1 justify-start gap-3 cursor-pointer flex-col 3sm:flex-row items-start 3sm:items-center ">
+					<div className="flex_center w-14 h-14 cursor-pointer rounded-full z-10 bg-white  min-w-[3.5rem] min-h-[3.5rem]">
 						<Image
 							alt={t("altProfilePicture")}
 							className="rounded-full object-contain"
@@ -39,50 +40,58 @@ export const PostCard: React.FC<Props> = ({ post, handleTagClick }) => {
 							width={45}
 						/>
 					</div>
-					<div className="flex flex-col">
+					<div className="flex flex-col max-w-[100%] overflow-hidden px-4 3sm:px-0">
 						<h3 className="font-satoshi font-semibold text-mlt-dark-2">{post?.creator?.name}</h3>
-						<p className="font-inter text-mlt-dark-6">{post?.creator?.email}</p>
+
+						<p className="font-inter text-mlt-dark-6 text-ellipsis overflow-hidden">
+							{post?.creator?.email}
+						</p>
 					</div>
 				</div>
-				<div
-					className="copy_btn"
-					// onClick={() => {
-					// 	// hello
-					// }}
-				>
-					<IconEmbedSVG
-						alt={t("altCopyPrompt")}
-						color1="mlt-orange-secondary"
-						color2="mlt-orange-dark"
-						height={34}
-						opacity1="84"
-						style={{ zIndex: 10 }}
-						type={copied ? "clipboard-check" : "clipboard"}
-						width={22}
-					/>
-				</div>
+
+				<IconEmbedSvgPop
+					c1="mlt-orange-secondary"
+					c2="mlt-orange-dark"
+					height={22}
+					op1="84"
+					style={{ position: "absolute", right: "0", top: "0" }}
+					text={t("altCopyPrompt")}
+					type={copied ? "clipboard-check" : "clipboard"}
+					width={22}
+					onClick={() => handleTagClick(post?.creator?.name)}
+				/>
 			</div>
 
-			<p>{post.prompt}</p>
-			<p>
-				{post.tags.map((tag: string, index) => (
-					<span
-						key={index}
-						className="mx-0.5 my-1 py-0.5 px-2 inline-block 
-						rounded-full bg-white/50 hover:bg-white/90 cursor-pointer
-						transition-colors duration-200 "
-					>
-						<IconEmbedSVG
-							color1="mlt-orange-secondary"
-							color2="mlt-orange-dark"
-							height={20}
-							opacity1="84"
-							style={{ zIndex: 10, display: "inline-block", marginRight: "3px" }}
-							type="tag"
-							width={20}
+			<div className="prompt_card_prompt">
+				<p>{post.prompt}</p>
+				{post?.link && (
+					<a href={post.link} rel="noreferrer" style={{ display: "table-cell" }} target="_blank">
+						<IconEmbedSvgPop
+							bgColor="bg-mlt-purple-secondary"
+							c1="mlt-purple-secondary"
+							c2="mlt-purple-secondary"
+							height={18}
+							op1="84"
+							style={{
+								width: "36px",
+								height: "36px",
+								position: "absolute",
+								float: "right",
+								right: "4px",
+								top: "4px",
+							}}
+							stylePosWrapper={{ transform: "translate(0, 0)" }}
+							text={t("altCopyPrompt")}
+							type={"message-lines"}
+							width={18}
+							wrapperClass="none"
 						/>
-						{tag}
-					</span>
+					</a>
+				)}
+			</div>
+			<p>
+				{(post.tags as string[]).map((tag: string, index) => (
+					<PostTag key={index} text={tag} />
 				))}
 			</p>
 		</div>
