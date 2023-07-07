439c2de4f07dc26d149949c325d1b9b32c12ad3f
Spas Z. Spasov <spas.z.spasov@metalevel.tech>
2023-07-07 12:10:01 +0300

    refactor: protect the app via the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,59 @@
-import createMiddleware from "next-intl/middleware";
+/**
+ * @see https://nextjs.org/docs/app/building-your-application/routing/middleware
+ * @see https://github.com/pillarjs/path-to-regexp#path-to-regexp-1
+ * @see https://next-intl-docs.vercel.app/docs/routing/middleware#example-auth-js
+ */
 
-export default createMiddleware({
-	// A list of all locales that are supported
-	// locales: ["en", "bg"],
-	locales: ["en"],
+import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
+import createIntlMiddleware from "next-intl/middleware";
+import { NextMiddlewareResult } from "next/dist/server/web/types";
+import { NextFetchEvent, NextRequest } from "next/server";
 
-	// If this locale is matched, pathnames work without a prefix (e.g. `/about`)
+const locales = ["en"]; // ["en", "bg"];
+const publicPages = ["/", "/login", "/privacy-policy", "/cookies-consent", "/terms-of-use"];
+
+const intlMiddleware = createIntlMiddleware({
+	locales,
 	defaultLocale: "en",
 });
 
+type AuthMiddleware = (
+	request: NextRequestWithAuth | NextRequest,
+	event?: NextFetchEvent
+) => Promise<NextMiddlewareResult>;
+
+const authMiddleware = withAuth(
+	// Note that this callback is only invoked if
+	// the `authorized` callback has returned `true`
+	// and not for pages listed in `pages`.
+	function onSuccess(req) {
+		return intlMiddleware(req);
+	},
+	{
+		callbacks: {
+			authorized: ({ token }) => token != null,
+		},
+		pages: {
+			// signIn: "/login",
+			signIn: "/api/auth/signin",
+		},
+	}
+) as AuthMiddleware;
+
+export default function middleware(req: NextRequest) {
+	const publicPathnameRegex = RegExp(
+		`^(/(${locales.join("|")}))?(${publicPages.join("|")})?/?$`,
+		"i"
+	);
+	const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
+
+	if (isPublicPage) {
+		return intlMiddleware(req);
+	} else {
+		return authMiddleware(req);
+	}
+}
+
 export const config = {
-	// Skip all paths that should not be internationalized
 	matcher: ["/((?!api|_next|.*\\..*).*)"],
 };
